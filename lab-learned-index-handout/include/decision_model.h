8e38971f27894e18beb963009c0eae6fa565e990
key4127 <2827092384@qq.com>
2025-03-03 16:09:49 +0800

    complete lab1: task3

--- lab-learned-index-handout/include/decision_model.h
+++ lab-learned-index-handout/include/decision_model.h
@@ -10,9 +10,25 @@ class DecisionTreeModel : public Model {
     virtual std::optional<ValueType> predict(KeyType key) const = 0;
   };
 
-  class LeafNode : public TreeNode {};
+  class LeafNode : public TreeNode {
+  public: 
+    std::vector<DataPoint> pairs;
 
-  class InternalNode : public TreeNode {};
+    std::optional<ValueType> predict(KeyType key) const {
+      
+    }
+  };
+
+  class InternalNode : public TreeNode {
+  public: 
+    int split_key;
+    std::unique_ptr<TreeNode> left;
+    std::unique_ptr<TreeNode> right;
+
+    std::optional<ValueType> predict(KeyType key) const {
+      
+    }
+  };
 
  private:
   double compute_split_loss(const std::vector<DataPoint>& data,
@@ -20,7 +36,32 @@ class DecisionTreeModel : public Model {
   double compute_variance(const std::vector<DataPoint>& data, size_t start,
                           size_t end);  // In README Others.
 
-  std::unique_ptr<TreeNode> build_tree(const std::vector<DataPoint>& data);
+  std::unique_ptr<TreeNode> build_tree(const std::vector<DataPoint>& data) {
+    return build_tree_helper(data, 0, data.size());
+  }
+
+  // do not contain end
+  std::unique_ptr<TreeNode> build_tree_helper(const std::vector<DataPoint>& data, int begin, int end) {
+    if (end - begin < default_max_leaf_samples) {
+      std::unique_ptr<LeafNode> r = std::make_unique<LeafNode>();
+
+      for (int i = begin; i < end; i++) {
+        r->pairs.insert(r->pairs.end(), data[i]);
+      }
+
+      return r;
+
+    } else {
+      std::unique_ptr<InternalNode> r = std::make_unique<InternalNode>();
+
+      int len = (end - begin) / 2;
+      r->split_key = 0;
+      r->left = build_tree_helper(data, begin, begin + len);
+      r->right = build_tree_helper(data, begin + len, end);
+
+      return r;
+    }
+  }
 
  public:
   DecisionTreeModel() {};
