f1072ab26229c4dae5de9b0a25dc0492e0514e08
key4127 <2827092384@qq.com>
2025-03-17 11:00:47 +0800

    complete hw2 cpp codes

--- hw2/main.cc
+++ hw2/main.cc
@@ -1,21 +1,52 @@
 #include <iostream>
+#include <array>
 #include "MurmurHash3.h"
 
 int main() {
-    uint64_t key = 522123456789;
-    uint64_t hash[2] = {0};
+    int n = 100;
+    int m[4] = {200, 300, 400, 500};
+    int false_num[5][4] = {0};
 
-    MurmurHash3_x64_128(&key, sizeof(key), 1, hash);
+    // k, m, n
+    bool hash[5][4][500] = {false};
+    std::array<uint32_t, 4> hash_out;
 
-    std::cout << "Hash value: ";
-    for(int i = 0; i < 2; i++) {
-        std::cout << hash[i] << " ";
+    for (int k = 0; k < 5; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int k1 = 0; k1 <= k; k1++) {
+                MurmurHash3_x64_128(&i, sizeof(i), k1 + 1, hash_out.data());
+                for (int j = 0; j < 4; j++) {
+                    int index = hash_out[0] % m[j];
+                    hash[k][j][index] = true;
+                }
+            }
+        }
     }
-    std::cout << std::endl;
-    //mod m-1
-    //only use hash[1] 
-    int m = 100;
-    std::cout << hash[1]%(m-1) << " ";
-    
+
+    for (int k = 0; k < 5; k++) {
+        for (int i = n; i < 2 * n; i++) {
+            for (int j = 0; j < 4; j++) {
+                bool false_element = true;
+                for (int k1 = 0; k1 <= k; k1++) {
+                    MurmurHash3_x64_128(&i, sizeof(i), k1 + 1, hash_out.data());
+                    int index = hash_out[0] % m[j];
+                    if (!hash[k][j][index]) {
+                        false_element = false;
+                        break;
+                    }
+                }
+                if (false_element) {
+                    false_num[k][j]++;
+                }
+            }
+        }
+    }
+
+    for (int k = 0; k < 5; k++) {
+        for (int j = 0; j < 4; j++) {
+            printf("k = %d, m/n = %d, result = %.3lf\n", k + 1, j + 2, (double)false_num[k][j] / n);
+        }
+    }
+
     return 0;
 }
